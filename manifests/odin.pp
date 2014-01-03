f2d0010ff867f70bad4376b39ee94e89c22873c0
Michael Rennecke <michael.rennecke@gmail.com>
2014-01-03 13:49:51 +0100

    add syntax highlighting for puppet in vim

--- manifests/odin.pp
+++ manifests/odin.pp
@@ -1,20 +1,48 @@
 
-file { '/usr/sbin/batched_discard':
-    ensure => present,
-    source => 'puppet:///modules/anacron/batched_discard',
-    owner  => 'root',
-    group  => 'root',
-    mode   => '550',
-    before => Class['anacron'],
-}
+node default {
+    
+    $packages = [ 'ntp', 'htop', 'vim-puppet']
+    package { $packages: 
+        ensure => 'installed'
+    }
 
-class { 'anacron':
-    jobs => {
-        'trim_ssd' => {
-            command => '/usr/sbin/batched_discard',
-            comment => 'weekly trim',
-            delay   => 5,
-            period  => '@weekly',
+    file { '/var/lib/vim/addons/syntax/':
+        ensure => directory,
+    }
+
+    file { '/var/lib/vim/addons/ftdetect/':
+        ensure => directory,
+    }
+
+    file { '/var/lib/vim/addons/syntax/puppet.vim':
+        ensure  => link,
+        target  => '/usr/share/vim/addons/syntax/puppet.vim',
+        require => [Package['vim-puppet'], File['/var/lib/vim/addons/syntax/'] ],
+    }
+
+    file { '/var/lib/vim/addons/ftdetect/puppet.vim':
+        ensure  => link,
+        target  => '/usr/share/vim/addons/ftdetect/puppet.vim',
+        require => [ Package['vim-puppet'], File['/var/lib/vim/addons/ftdetect'], ],
+    }
+
+    file { '/usr/sbin/batched_discard':
+        ensure => present,
+        source => 'puppet:///modules/anacron/batched_discard',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '550',
+        before => Class['anacron'],
+    }
+    
+    class { 'anacron':
+        jobs => {
+            'trim_ssd' => {
+                command => '/usr/sbin/batched_discard',
+                comment => 'weekly trim',
+                delay   => 5,
+                period  => '@weekly',
+            }
         }
     }
 }
